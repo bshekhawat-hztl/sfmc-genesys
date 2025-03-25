5de7de40b16cc41c2300b7cb097b8479d71ae4b9
bshekhawat-hztl <bshekhawat@horizontal.com>
2025-03-25 16:27:15 +0400

    Updated html

--- custom_activity.js
+++ custom_activity.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
